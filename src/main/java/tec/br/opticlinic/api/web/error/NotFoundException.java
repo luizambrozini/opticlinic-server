f1cb9a2d83cacb90e6b248194437573ab1f0a4f7
LuizAntonioMoreauAmb <luizambrozini@gmail.com>
2025-10-17 14:38:30 -0300

    Fix - fixed uncreated ConflictException BadRequestException UnprocessableEntityException on NotFoundException

--- src/main/java/tec/br/opticlinic/api/web/error/NotFoundException.java
+++ src/main/java/tec/br/opticlinic/api/web/error/NotFoundException.java
@@ -6,22 +6,24 @@ public class NotFoundException extends AppException {
     public NotFoundException(ErrorCode code, String message) {
         super(HttpStatus.NOT_FOUND, code, message);
     }
-}
 
-public class ConflictException extends AppException {
-    public ConflictException(ErrorCode code, String message) {
-        super(HttpStatus.CONFLICT, code, message);
+/*
+    public class ConflictException extends AppException {
+        public ConflictException(ErrorCode code, String message) {
+            super(HttpStatus.CONFLICT, code, message);
+        }
     }
-}
 
-public class BadRequestException extends AppException {
-    public BadRequestException(ErrorCode code, String message) {
-        super(HttpStatus.BAD_REQUEST, code, message);
+    public class BadRequestException extends AppException {
+        public BadRequestException(ErrorCode code, String message) {
+            super(HttpStatus.BAD_REQUEST, code, message);
+        }
     }
-}
 
-public class UnprocessableEntityException extends AppException {
-    public UnprocessableEntityException(ErrorCode code, String message) {
-        super(HttpStatus.UNPROCESSABLE_ENTITY, code, message);
+    public class UnprocessableEntityException extends AppException {
+        public UnprocessableEntityException(ErrorCode code, String message) {
+            super(HttpStatus.UNPROCESSABLE_ENTITY, code, message);
+        }
     }
+ */
 }
